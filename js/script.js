c069346b16ca536897fa6faa7e8e283ec21509a1
Pietro Chiarva <pietro.chiarva@edu.itspiemonte.it>
2017-12-03 23:35:34 +0100

    mok

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ $.ajax({
 	$("#container").empty();
 	createAllCollapsiblePanel(allDetectionData);
 	assignCollapsibleClick();
-	addEventListenerToCollapse();
+	//addEventListenerToCollapse();
 
 })
 .fail(function(error) {
@@ -46,7 +46,6 @@ function assignCollapsibleClick(){
      */
 	var acc = document.getElementsByClassName("panelHeader");
 	for (var i = 0; i < acc.length; i++) {
-		console.log(acc[i]);
 		acc[i].onclick = function() {
 	    	this.classList.toggle("active");
 	    	var panel = this.nextElementSibling;
@@ -59,15 +58,17 @@ function assignCollapsibleClick(){
 	}
 }
 
-
+var ind = [];
 /**
  * [addEventListenerToCollapse function that retrive index of clicked collapse]
  */
 function addEventListenerToCollapse() {
 	$('.collapse').click(function (e){
-		console.log($(this).index('.collapse'));
+		ind.push(($(this).index('.collapse')));
+
 	});
 }
+
 
 
 /**
@@ -115,6 +116,16 @@ function createCollapsiblePanel(detectedDataForSinglelocation) {
     var divPanelCollapsibleBody = createPanelBody(detectedDataForSinglelocation);
 	collapse.append(divPanelHeader,divPanelCollapsibleBody);
 	allCollapsibleContainer.append(collapse);
+
+	var index = collapse.index(".collapse");
+for(var y=0;y<ind.length;y++)
+{
+	if(index == ind[y])
+	{
+		console.log("mokkk");
+		divPanelCollapsibleBody.css("max-height", "353px");
+	}
+}
 }
 /**
  * [create the header of each collapsible panel]
@@ -127,7 +138,7 @@ function createPanelHeader(detectedDataForSinglelocation){
 										  + detectedDataForSinglelocation.station.city +
 										  " | Temperature: "+ detectedDataForSinglelocation.temperature)
 										  .append(getFlagNation(detectedDataForSinglelocation));
-  divPanelHeader.nextElementSibling.style.maxHeight = panel.scrollHeight + "px";
+
 
 
     return divPanelHeader;
@@ -141,8 +152,8 @@ function createPanelHeader(detectedDataForSinglelocation){
 function createPanelBody(detectedDataForSinglelocation){
 	var divPanelCollapsibleBody = $('<div></div>').addClass("panelCollapsibleBody").html(detectedDataForSinglelocation.station.name +" Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui dosto odio dignissimos ducimus, qui blanditiis praesentium voluptatum deleniti atque corrupti, quos dolores et quas molestias excepturi sint, obcaecati cupiditate non provident, similique sunt in culpa, qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio, cumque nihil impedit, quo minus id, quod maxime placeat, facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet, ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem ipsum, quia dolor sit, amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt, ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit, qui in ea voluptate velit esse, quam nihil molestiae consequatur, vel illum, qui dolorem eum fugiat, quo voluptas nulla pariatur? [33] At vero eos et accusamus et iusto odio dignissimos ducimus, qui blanditiis praesentium voluptatum deleniti atque corrupti, quos dolores et quas molestias excepturi sint, obcaecati cupiditate non provident, similique sunt in culpa, qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio, cumque nihil impedit, quo minus id, quod maxime placeat, facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet, ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat");
 
-	/*var index = $(".panelCollapsibleBody").index('.collapse');
-	index.css("max-height": index.css("scroll-height")); */
+
+
 	return divPanelCollapsibleBody;
 }
 /**
